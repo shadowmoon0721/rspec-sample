98d10c63da14bb2267b8b3514ff3781d4e202a9f
shadowmoon0721 <shadowmoon0721@gmail.com>
2023-10-22 05:29:58 +0900

    5-4 コントローラーのテストコードを追加する

--- spec/requests/gym_enquetes_spec.rb
+++ spec/requests/gym_enquetes_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "GymEnquetes", type: :request do
-  describe "GET /gym_enquetes" do
-    it "works! (now write some real specs)" do
-      get gym_enquetes_path
-      expect(response).to have_http_status(200)
+  describe '正常' do
+    it "ページが表示されないこと" do
+      get "/gym_enquetes/new" 
+      expect(response).to have_http_status(404)
     end
   end
 end
