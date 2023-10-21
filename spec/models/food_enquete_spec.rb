a173bdd562eed9103b28410f721c03ad2d58c2fe
shadowmoon0721 <shadowmoon0721@gmail.com>
2023-10-22 02:06:09 +0900

    3-13 共通バリデーションのテストコードを書く

--- spec/models/food_enquete_spec.rb
+++ spec/models/food_enquete_spec.rb
@@ -31,53 +31,6 @@ RSpec.describe FoodEnquete, type: :model do
     end
   end
 
-  describe '入力項目の有無' do
-    # [Point.3-11-2]インスタンスを共通化してテストデータを作成します。
-    let(:new_enquete) { FoodEnquete.new }
-
-    context '必須入力であること' do
-      # [Point.3-4-1]itを複数書くことができます。
-      it 'お名前が必須であること' do
-        # [Point.3-4-2]バリデーションエラーが発生することを検証します。
-        expect(new_enquete).not_to be_valid
-        # [Point.3-4-3]必須入力のメッセージが含まれることを検証します。
-        expect(new_enquete.errors[:name]).to include(I18n.t('errors.messages.blank'))
-      end
-
-      it 'メールアドレスが必須であること' do
-        expect(new_enquete).not_to be_valid
-        expect(new_enquete.errors[:mail]).to include(I18n.t('errors.messages.blank'))
-      end
-
-      # [Point.3-4-1]itを複数書くことができます。
-      it '登録できないこと' do
-        # [Point.3-4-4]保存に失敗することを検証します。
-        expect(new_enquete.save).to be_falsey
-      end
-    end
-
-    context '任意入力であること' do
-      it 'ご意見・ご要望が任意であること' do
-        expect(new_enquete).not_to be_valid
-        # [Point.3-4-6]必須入力のメッセージが含まれないことを検証します。
-        expect(new_enquete.errors[:request]).not_to include(I18n.t('errors.messages.blank'))
-      end
-    end
-  end
-
-  describe 'メールアドレスの形式' do
-    context '不正な形式のメールアドレスの場合' do
-      it 'エラーになること' do
-        new_enquete = FoodEnquete.new
-        # [Point.3-7-1]不正な形式のメールアドレスを入力します。
-        new_enquete.mail = "taro.tanaka"
-        expect(new_enquete).not_to be_valid
-        # [Point.3-7-2]不正な形式のメッセージが含まれることを検証します。
-        expect(new_enquete.errors[:mail]).to include(I18n.t('errors.messages.invalid'))
-      end
-    end 
-  end
-
   describe 'アンケート回答時の条件' do
     context 'メールアドレスを確認すること' do
 
@@ -140,6 +93,11 @@ RSpec.describe FoodEnquete, type: :model do
       # [Point.3-5-2]成人になることを検証します。
       expect(foodEnquete.send(:adult?, 20)).to be_truthy
     end 
+  end
+
+  describe '共通バリデーション' do
+    it_behaves_like '入力項目の有無'
+    it_behaves_like 'メールアドレスの形式'
   end
 
   describe '共通メソッド' do
